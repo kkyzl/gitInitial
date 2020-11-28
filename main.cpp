a47b7791a7b6bb94c0139e5b7bc53433e1975782
YinZi Lu <b09901042@ntu.edu.tw>
2020-11-28 15:34:13 +0800

    finish gender

--- main.cpp
+++ main.cpp
@@ -4,10 +4,20 @@ using namespace std;
 
 int main(){
 	string name = "";
+	char gender = '\0';
 	cout << "please enter your name?" << endl;
 	cin >> name;
 	
 	cout<< "hello " << name << endl;
+	cout << "please enter your gender?(M/F)" << endl;
+	cin >> gender;
+	if(gender == 'M' || gender == 'm'){
+		cout << "hi Mr. " << name << "." << endl;
+	}else if(gender == 'F' || gender == 'f'){
+		cout << "Hi Ms. " << name << "." << endl;
+	}else{
+		cout << "Hi " << name << "." << endl;
+	}
 	system("pause");
 	return 0;
 }
